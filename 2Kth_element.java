5ea7cc4aaaed45dc28192f273fcde59e8f3c79e4
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-07 22:45:49 +0530

    Fix comment punctuation in 2Kth_element.java

--- 2Kth_element.java
+++ 2Kth_element.java
@@ -1,5 +1,5 @@
 
-//this first solution of kth element the time complexity of this solution O(Nlogn)
+//this first solution of kth element the time complexity of this solution O(Nlogn).
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
