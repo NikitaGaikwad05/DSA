8167594cdda039fccd82d07cb06c17a7d5977578
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-25 19:55:11 +0530

    Add pattern printing method in pattern2 class
    Implement a method to print a pattern of asterisks.

---
+++ pattern2.java
@@ -0,0 +1,25 @@
+*/ *
+   **
+   ***
+   ****
+   *****  */
+
+  import java.io.*;
+public class pattern2{
+    public void pattern(int n)
+    {
+        for(int i=0;i<n;i++)
+            {
+            for(int j=0;j<=i;j++)
+                {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        pattern2 pt=new pattern2();
+        int n=5;
+        pt.pattern(n);
+    }
+}
