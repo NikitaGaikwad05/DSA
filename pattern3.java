827d2ee79ed12a6b0d04603fc261fbf10c178073
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-25 20:02:57 +0530

    Add pattern printing method in pattern3 class
    Implement a method to print a number pattern.

---
+++ pattern3.java
@@ -0,0 +1,26 @@
+*/ 1
+   12   
+   123  
+   1234 
+   12345 */
+
+  import java.io.*;
+public class pattern3{
+    public void pattern(int n)
+    {
+        for(int i=1;i<=5;i++)
+            {
+            for(int j=1;j<=i;j++)
+                {
+                System.out.print(j);
+
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        pattern3 pt=new pattern3();
+        int n=5;
+        pt.pattern(n);
+    }
+}
