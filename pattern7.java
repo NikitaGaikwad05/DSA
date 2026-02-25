7a547191391eca8a7e52b9bba0aa10b063eebfdd
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-25 22:55:05 +0530

    Create pattern7.java

---
+++ pattern7.java
@@ -0,0 +1,30 @@
+*/  *   
+   ***
+  *****
+ *******
+*********    */
+import java.io.*;
+public class pattern7{
+    public void pattern(int n)
+    {
+        for(int i=0;i<n;i++)
+            {
+            for(int j=0;j<n-i-1;j++)
+                {
+                System.out.println("");
+            }
+            for(int j=0;j<2*i+1;j++)
+                {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String args[]){
+        pattern7 pt=new pattern7();
+        int n=5;
+        pt.pattern(n);
+    }
+}
