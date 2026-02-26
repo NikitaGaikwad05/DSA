e24f4c3b688f8e2441f94012f6a231ca7cf6cf98
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-26 18:13:12 +0530

    Create demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+//input and output in c++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter the value of x and y";
+    cin>>x>>y;
+    cout<<"x="<<x<<"y="<<y;
+    return 0;
+}
