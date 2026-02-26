e1387edf84041952e8b6b84660042a6fcd3dfe7a
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-26 18:14:23 +0530

    Create second.cpp

---
+++ second.cpp
@@ -0,0 +1,14 @@
+// show the use of if else statement
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Ente the age of person";
+    cin>>age;
+    if(age>=18){
+        cout<<"You are an adult:"<<age;
+    }
+    else{
+        cout<<"you are not an adult";
+    }
+}
